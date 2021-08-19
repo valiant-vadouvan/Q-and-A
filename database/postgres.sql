f64e2a8a8b8fb4d5c5584ecd683da3bd6d395d9d
Sean Park <seansangpark@gmail.com>
2021-08-19 15:47:43 -0700

    Modified sql file

--- database/postgres.sql
+++ database/postgres.sql
@@ -1,4 +1,7 @@
 CREATE DATABASE q_and_a;
+
+\c q_and_a
+
 CREATE TABLE IF NOT EXISTS questions (
   ID BIGSERIAL NOT NULL PRIMARY KEY,
   product_id INT NOT NULL,
@@ -6,9 +9,10 @@ CREATE TABLE IF NOT EXISTS questions (
   date_written BIGINT NOT NULL,
   asker_name VARCHAR(30) NOT NULL,
   asker_email VARCHAR(50) NOT NULL,
-  reported BOOLEAN,
-  helpfulness INT
+  reported BOOLEAN DEFAULT FALSE,
+  helpfulness INT DEFAULT 0
 );
+
 CREATE TABLE IF NOT EXISTS answers (
   ID BIGSERIAL NOT NULL PRIMARY KEY,
   questions_id INT NOT NULL,
@@ -16,10 +20,11 @@ CREATE TABLE IF NOT EXISTS answers (
   date_written BIGINT NULL,
   answerer_name VARCHAR(30) NOT NULL,
   answerer_email VARCHAR(50) NOT NULL,
-  reported BOOLEAN,
-  helpfulness INT,
+  reported BOOLEAN DEFAULT FALSE,
+  helpfulness INT DEFAULT 0,
   FOREIGN KEY (questions_id) REFERENCES questions(id)
 );
+
 CREATE TABLE IF NOT EXISTS photos (
   ID BIGSERIAL NOT NULL PRIMARY KEY,
   answer_id INT NOT NULL,
@@ -27,30 +32,12 @@ CREATE TABLE IF NOT EXISTS photos (
   FOREIGN KEY (answer_id) REFERENCES answers(id)
 );
 -- load database
-COPY questions (
-  id,
-  product_id,
-  body,
-  date_written,
-  asker_name,
-  asker_email,
-  reported,
-  helpfulness
-)
-FROM '/Users/seanpark/desktop/questions.csv' DELIMITER ',' CSV HEADER;
-COPY answers (
-  id,
-  questions_id,
-  body,
-  date_written,
-  answerer_name,
-  answerer_email,
-  reported,
-  helpfulness
-)
-FROM '/Users/seanpark/desktop/answers.csv' DELIMITER ',' CSV HEADER;
-COPY photos(id, answer_id, photo_url)
-FROM '/Users/seanpark/desktop/answers_photos.csv' DELIMITER ',' CSV HEADER;
+COPY questions (id, product_id, body, date_written, asker_name, asker_email, reported, helpfulness) FROM '/Users/seanpark/desktop/questions.csv' DELIMITER ',' CSV HEADER;
+
+COPY answers (id, questions_id, body, date_written, answerer_name, answerer_email, reported, helpfulness) FROM '/Users/seanpark/desktop/answers.csv' DELIMITER ',' CSV HEADER;
+
+COPY photos(id, answer_id, photo_url) FROM '/Users/seanpark/desktop/answers_photos.csv' DELIMITER ',' CSV HEADER;
+
 -- CREATE INDEX
 CREATE INDEX productID on questions(product_id);
 CREATE INDEX questionID on questions(id);
@@ -58,37 +45,8 @@ CREATE INDEX answerID on answers(id);
 CREATE INDEX questionsID on answers(questions_id);
 CREATE INDEX answersID on photos(answer_id);
 CREATE INDEX photoID on photos(id);
+
 -- INCREMENT NEW ID BY ONE AFTER POST REQUEST
-SELECT pg_catalog.setval(
-    pg_get_serial_sequence('questions', 'id'),
-    (
-      SELECT MAX(id)
-      FROM questions
-    ) + 1
-  );
-SELECT pg_catalog.setval(
-    pg_get_serial_sequence('answers', 'id'),
-    (
-      SELECT MAX(id)
-      FROM answers
-    ) + 1
-  );
-SELECT pg_catalog.setval(
-    pg_get_serial_sequence('photos', 'id'),
-    (
-      SELECT MAX(id)
-      FROM photos
-    ) + 1
-  );
-ALTER TABLE questions
-ALTER COLUMN date_written TYPE BIGINT USING date_written::bigint;
-ALTER TABLE answers
-ALTER COLUMN date_written TYPE BIGINT USING date_written::bigint;
--- CREATE INDEX productId_index ON questions (question)
--- UPDATE questions SET reported = 'true' WHERE reported = 1;
--- CREATE INDEX productID_index ON reviews (product_id);
--- ALTER TABLE skus ADD INDEX style_skus (styleId);
--- CREATE INDEX [index name] ON [table name] (COLUMN NAME);
-DROP TABLE photos;
-DROP TABLE answers;
-DROP TABLE questions;
+SELECT pg_catalog.setval(pg_get_serial_sequence('questions', 'id'),(SELECT MAX(id)FROM questions) + 1);
+SELECT pg_catalog.setval(pg_get_serial_sequence('answers', 'id'),(SELECT MAX(id)FROM answers) + 1);
+SELECT pg_catalog.setval(pg_get_serial_sequence('photos', 'id'),(SELECT MAX(id)FROM photos) + 1);
