25a0a9d892a6c09c33af15e906c19ca16dabe5bb
Sean Park <seansangpark@gmail.com>
2021-08-18 18:58:21 -0700

    Stress test conducted from local machine

--- test.js
+++ test.js
@@ -6,25 +6,78 @@ import { sleep } from 'k6';
 
 export let options = {
   // vus: 100,
-  // duration: '3s',
-  // stages: [
-  //   { duration: '1m', target: 10 },
-  //   { duration: '10m', target: 10 },
-  //   { duration: '1m', target: 0 },
-  // ]
+  // duration: '1s',
+
+  stages: [
+    { duration: '1s', target: 10 },
+    { duration: '2s', target: 100 },
+    { duration: '1s', target: 1 },
+  ]
 
   // Stress test
-  stages: [
-    { duration: '2m', target: 100 }, // below normal load
-    { duration: '5m', target: 100 },
-    { duration: '2m', target: 200 }, // normal load
-    { duration: '5m', target: 200 },
-    { duration: '2m', target: 300 }, // around the breaking point
-    { duration: '5m', target: 300 },
-    { duration: '2m', target: 400 }, // beyond the breaking point
-    { duration: '5m', target: 400 },
-    { duration: '10m', target: 0 }, // scale down. Recovery stage.
-  ]
+  // base line
+  // stages: [
+  //   { duration: '1m', target: 10 },
+  //   { duration: '2m', target: 1000 },
+  //   { duration: '1m', target: 10 },
+  // ]
+
+  // load test
+  // stages: [
+  //   { duration: '5m', target: 2000 },
+  //   { duration: '15m', target: 2000 },
+  //   { duration: '5m', target: 0 },
+  // ]
+
+  // spite test
+  // stages: [
+  //   { duration: '1m', target: 2000 },
+  //   { duration: '9m', target: 2000 },
+  //   { duration: '3m', target: 10000 },
+  //   { duration: '7m', target: 10000 },
+  //   { duration: '10m', target: 0 },
+  // ],
+
+  // stages: [
+  //   { duration: '30s', target: 2000 },
+  //   { duration: '540s', target: 2000 },
+  //   { duration: '180s', target: 10000 },
+  //   { duration: '420s', target: 10000 },
+  //   { duration: '300s', target: 0 },
+  // ],
+
+  // peak test
+  // stages: [
+  //   { duration: '5m', target: 100 },
+  //   { duration: '10m', target: 100 },
+  //   { duration: '5m', target: 0 },
+  // ],
+  // thresholds: {
+  //   http_req_duration: ['p(99)<150'],
+  //   // 99% of requests must complete below 150ms
+  // },
+
+  // soak test
+  // stages: [
+  //   { duration: '2m', target: 400 },
+  //     // ramp up to 400 users
+  //   { duration: '3h56m', target: 400 },
+  //     // stay at 400 for ~4 hours
+  //   { duration: '2m', target: 0 },
+  //     // scale down (optional)
+  // ]
+
+  // stages: [
+  //   { duration: '2m', target: 100 }, // below normal load
+  //   { duration: '5m', target: 100 },
+  //   { duration: '2m', target: 200 }, // normal load
+  //   { duration: '5m', target: 200 },
+  //   { duration: '2m', target: 300 }, // around the breaking point
+  //   { duration: '5m', target: 300 },
+  //   { duration: '2m', target: 400 }, // beyond the breaking point
+  //   { duration: '5m', target: 400 },
+  //   { duration: '10m', target: 0 }, // scale down. Recovery stage.
+  // ]
 };
 
 export default function () {
@@ -35,8 +88,8 @@ export default function () {
   http.batch([
     ['GET', `${API_BASE_URL}/qa/questions`],
     ['GET', `${API_BASE_URL}/qa/questions/1/answers`],
-    ['POST', `${API_BASE_URL}/qa/questions/?body=this is test 123&name=test123&email=test123@test123.com&product_id=1`],
-    ['POST', `${API_BASE_URL}/qa/questions/?body=this is test 123&name=test123&email=test123@test123.com&photos=URLfortesting123`],
+    ['POST', `${API_BASE_URL}/qa/questions/`],
+    ['POST', `${API_BASE_URL}/qa/questions/1/answers`],
     ['PUT', `${API_BASE_URL}/qa/questions/1/helpful`],
     ['PUT', `${API_BASE_URL}/qa/questions/1/report`],
     ['PUT', `${API_BASE_URL}/qa/answers/1/helpful`],
